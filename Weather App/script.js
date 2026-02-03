609f3cc026d1c1078aabfeb55a2de33f05a7167e
Parth Kshirsagar <parthyk007@gmail.com>
2026-02-03 23:27:13 +0530

    minor changes to weather app

--- Weather App/script.js
+++ Weather App/script.js
@@ -7,36 +7,57 @@ const inputElem = document.querySelector("input");
 const buttonElem = document.querySelector("#search");
 const error = document.querySelector(".error-message");
 
-async function main() {
-    const location = inputElem.value.trim();
-
-    //call the API ->
+const fetchWeather = async (location) => {
     const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${location}`)
     const data = await response.json();
 
     if(response.status == 400){
         error.textContent = "Invalid Location";
+    } else if(response.status == 200){
+        error.textContent = "";
+        return data
     }
+}
+
+const updateDOM = (temp, city, country, time, date, condition, conditionImg) => {
+    const tempEle = document.querySelector(".temperature");
+    const locEle = document.querySelector(".location");
+    const timeEle = document.querySelector(".time");
+    const dateEle = document.querySelector(".date");
+    const conditionEle = document.querySelector(".condition");
+    const conditionImgEle = document.querySelector("#condition-img");
+    
+    tempEle.textContent = `${temp}°C`;
+    locEle.textContent = city + ", " + country;
+    timeEle.textContent = time;
+    dateEle.textContent = date;
+    conditionEle.textContent = condition;
+    conditionImgEle.setAttribute("src", conditionImg);
+    errorEle.textContent = ""
+}
+
+async function main() {
+    const location = inputElem.value.trim();
+
+    //call the API ->
+    const data = await fetchWeather(location);
 
     //update the DOM
-    const temp = document.querySelector(".temperature");
-    const loc = document.querySelector(".location");
-    const time = document.querySelector(".time");
-    const date = document.querySelector(".date");
-    const condition = document.querySelector(".condition");
-    const conditionImg = document.querySelector("#condition-img");
-
-    if(response.status == 200){
-        temp.textContent = `${data.current.temp_c}°C`;
-        loc.textContent = data.location.name + ", " + data.location.country;
-        time.textContent = data.location.localtime.split(" ")[1];
-        date.textContent = data.location.localtime.split(" ")[0];
-        condition.textContent = data.current.condition.text;
-        error.textContent = ""
-        conditionImg.setAttribute("src", data.current.condition.icon);
-    }
+    updateDOM(
+        data.current.temp_c, 
+        data.location.name,
+        data.location.country,
+        data.location.localtime.split(" ")[1],
+        data.location.localtime.split(" ")[0],
+        data.current.condition.text,
+        data.current.condition.icon
+    )
 
     inputElem.value="";
 }
-buttonElem.addEventListener("click", main);
 
+inputElem.value = "Delhi India";
+main();
+inputElem.value = "";
+
+buttonElem.addEventListener("click", main);

d8e85bc36df5d3a3f1c17a1c5c27ec69866cfca9
Parth Kshirsagar <parthyk007@gmail.com>
2026-03-17 12:00:41 +0530

    add array methods class

---
+++ react-2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ArrayMethods from './components/ArrayMethods'
+
+function App() {
+
+  return (
+    <>
+      <ArrayMethods />      
+    </>
+  )
+}
+
+export default App
